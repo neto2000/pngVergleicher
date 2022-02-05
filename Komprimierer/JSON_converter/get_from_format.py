606809d28c766d2af5ad665351d1754aaac85249
neto2000 <82058954+neto2000@users.noreply.github.com>
2022-02-05 15:24:46 +0100

    universal file path

--- Komprimierer/JSON_converter/get_from_format.py
+++ Komprimierer/JSON_converter/get_from_format.py
@@ -1,9 +1,19 @@
+from pathlib import Path
+
 
 def Get(file):
 
+    repo_path = Path(__file__).resolve().parent.parent.parent
+
+    lib_file = Path(file)
+
+    uni_file = repo_path / lib_file
+
+    print(uni_file)
+
     try:
     
-        Kaze_file = open(file, "r")
+        Kaze_file = open(uni_file, "r")
 
         Kaze_file.close()
 
@@ -14,14 +24,15 @@ def Get(file):
         print("could not find the file")
 
 
-    Kaze_file = open(file, "r")
+    Kaze_file = open(uni_file, "r")
 
     lines = Kaze_file.readlines()
 
     Kaze_file.close()
 
-    print(lines)
+    print(lines[1])
 
+print(Path(__file__).resolve().parent.parent.parent)
 
-Get("D:\Dokumente\Scripts\GitHub\pngVergleicher\Komprimierer\hash.json")
+Get("Komprimierer/hash.json")
 
