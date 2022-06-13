27fdf5c2de5f9afdfdd9bded62761647c8e7967a
neto2000 <82058954+neto2000@users.noreply.github.com>
2022-06-13 21:16:20 +0200

    created a scripts that can recognize a push on an arduino button

---
+++ arduino_key_recognizer/ahk_scripts/ahk_script7.ahk
@@ -0,0 +1,16 @@
+Loop, 3
+    InitOnlyOnce(A_Index)
+Return
+    
+InitOnlyOnce(Index)
+{
+  static init=0, text
+  
+  If !(init++){
+	;here comes the code\/
+	
+      	
+  }
+
+  
+}
---
+++ arduino_key_recognizer/start_ahk_file.py
@@ -0,0 +1,6 @@
+import os
+
+def start(file_name):
+
+    os.system("F:\\Scripts\\AHK\\AutoHotkeyInstall\\AutoHotkeyU64.exe F:\\Scripts\\Python\\Info\\ahk_scripts_auto\\" + file_name)
+
